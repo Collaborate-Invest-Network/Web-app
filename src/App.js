f271b7558e71bf7c1a55e7011fcf11c935a691bd
Gia Bao <21127017@student.hcmus.edu.vn>
2024-03-06 17:42:21 +0700

    test signin

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './pages/signup';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
+
